9200a2d9b4ebacc531bc8a8a67878788f7cc1f38
Paul Brown <paul@localhost.localdomain>
2016-08-18 11:03:17 +1000

    first commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM centos:latest
+
+RUN yum upgrade -y -q; yum clean all
+RUN yum install -y -q java-headless tar wget; yum clean all
+
+EXPOSE 2181 2888 3888
+
+ENV ZK_VERSION 3.5.2-alpha
+
+RUN wget -q http://mirror.ox.ac.uk/sites/rsync.apache.org/zookeeper/zookeeper-${ZK_VERSION}/zookeeper-${ZK_VERSION}.tar.gz -O - | tar -xzf -; mv zookeeper-${ZK_VERSION} /zookeeper
+
+VOLUME /data
+WORKDIR /zookeeper
+COPY zoo.cfg /zookeeper/conf/zoo.cfg
+COPY run.sh /run.sh
+
+CMD /run.sh
---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+: ${ZK_MYID:=1}
+: ${ZK_NUMBER_OF_NODES:=1}
+: ${ZK_SERVICE_NAME:=zookeeper}
+
+# We do not want to override the dynamic config file.
+if [[ ! -f /zookeeper/conf/zoo_dynamic.cfg ]]; then
+  for n in $(seq 1 ${ZK_NUMBER_OF_NODES}); do
+    echo "server.${n}:${ZK_SERVICE_NAME}-${n}:2888:3888:participant;2181" >> /zookeeper/conf/zoo_dynamic.cfg
+  done
+fi
+
+echo ${ZK_MYID} > /data/myid
+
+exec /zookeeper/bin/zkServer.sh start-foreground /zookeeper/conf/zoo.cfg
