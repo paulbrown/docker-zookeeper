90d232831df3c69bfc05572d8a4d485cb1b9b663
paulbrown <paul6rown@gmail.com>
2016-08-25 08:54:03 +1000

    change

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,8 @@
 FROM docker.io/paulbrown/base:latest
 
-RUN yum upgrade --quiet --assumeyes
+RUN yum upgrade --quiet --assumeyes && yum clean all
 
-RUN yum install --quiet --assumeyes java-headless
-
-RUN yum clean all
+RUN yum install --quiet --assumeyes java-headless && yum clean all
 
 RUN curl --silent --show-error http://apache.mirror.digitalpacific.com.au/zookeeper/zookeeper-3.5.2-alpha/zookeeper-3.5.2-alpha.tar.gz | tar -xzf - -C /opt
 
--- run.sh
+++ run.sh
@@ -6,10 +6,7 @@ POS=`expr index "$HOSTNAME" -`
 
 MYID=${HOSTNAME:POS:LENGTH-POS}
 
-exec /opt/zookeeper/bin/zkServer-initialize.sh --force
+/opt/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
+echo "server.$MYID=$HOSTNAME:2888:3888:participant;2181" >> /opt/zookeeper/conf/zoo.cfg.dynamic
+/opt/zookeeper/bin/zkServer.sh start-foreground /opt/zookeeper/conf/zoo.cfg
 
-echo "$MYID" >> /data/myid
-echo "server.$MYID=$HOSTNAME:2888:3888:observer;2181" >> /opt/zookeeper/conf/zoo_dynamic.cfg
-
-exec /opt/zookeeper/bin/zkServer.sh start /opt/zookeeper/conf/zoo.cfg
-exec /opt/zookeeper/bin/zkCli.sh reconfig -add "server.$MYID=$HOSTNAME:2888:3888:participant;2181"
