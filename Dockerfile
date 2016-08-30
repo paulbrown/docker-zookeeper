1601155a8508d6dd9c0c0c0851d55cfc40b4de4d
paulbrown <paul6rown@gmail.com>
2016-08-31 08:24:11 +1000

    change

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM docker.io/paulbrown/base:latest
 
 RUN yum upgrade --quiet --assumeyes && yum clean all
 
-RUN yum install --quiet --assumeyes java-headless && yum clean all
+RUN yum install --quiet --assumeyes java-headless bind-utils && yum clean all
 
 RUN curl --silent --show-error http://apache.mirror.digitalpacific.com.au/zookeeper/zookeeper-3.5.2-alpha/zookeeper-3.5.2-alpha.tar.gz | tar -xzf - -C /opt
 
@@ -15,8 +15,6 @@ EXPOSE 2181 2888 3888
 VOLUME /data
 
 COPY onStart.sh /onStart.sh
-
-RUN chmod +x /onStart.sh && ./onStart.sh
 
 
 
--- onStart.sh
+++ onStart.sh
@@ -5,7 +5,13 @@ LENGTH=${#HOSTNAME}
 POS=`expr index "${HOSTNAME}" -`
 MYID=${HOSTNAME:POS:LENGTH-POS}
 
-echo "server.${MYID}=localhost:2888:3888:participant;2181" >> /opt/zookeeper/conf/zoo.cfg.dynamic
+
+if [ ! -f /opt/zookeeper/conf/zoo.cfg.dynamic ]; then
+  if ! [[ ${MYID} =~ ^[0-9]+$ ]]; then
+    MYID="0"
+  fi 
+  echo "server.${MYID}=${HOSTNAME}:2888:3888;2181" >> /opt/zookeeper/conf/zoo.cfg.dynamic
+fi
 
 /opt/zookeeper/bin/zkServer-initialize.sh --force --myid=${MYID}
-/opt/zookeeper/bin/zkServer.sh start-foreground /opt/zookeeper/conf/zoo.cfg
+/opt/zookeeper/bin/zkServer.sh start /opt/zookeeper/conf/zoo.cfg
