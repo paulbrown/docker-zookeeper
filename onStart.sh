164853d6740e04cad14eaf42f313ba6158d064f8
paulbrown <paul6rown@gmail.com>
2016-09-05 14:42:31 +1000

    change

--- onStart.sh
+++ onStart.sh
@@ -11,17 +11,11 @@ for PEER in "${PEERS[@]}"
 do
   P_HOSTNAME=$( echo ${PEER} | cut -d "." -f1 )
   P_ID=$( echo ${P_HOSTNAME} | cut -d "-" -f2 | cut -d "-" -f1 )
-  if [ "${T_HOSTNAME}" == "${P_HOSTNAME}" ]; then
-    T_CONFIG="server.${P_ID}=${PEER}:2888:3888:participant;2181"
-  else
-    echo "server.${P_ID}=${PEER}:2888:3888:participant;2181" >> /opt/zookeeper/conf/zoo.cfg.dynamic
-  fi
+  echo "server.${P_ID}=${PEER}:2888:3888:participant;2181" >> /opt/zookeeper/conf/zoo.cfg.dynamic
 done
 
 /opt/zookeeper/bin/zkServer-initialize.sh --force --myid=${T_ID}
-echo ${T_CONFIG} >> /opt/zookeeper/conf/zoo.cfg.dynamic
 /opt/zookeeper/bin/zkServer.sh start /opt/zookeeper/conf/zoo.cfg
-/opt/zookeeper/bin/zkCli.sh reconfig -add ${T_CONFIG} quit
+/opt/zookeeper/bin/zkCli.sh reconfig -file /opt/zookeeper/conf/zoo.cfg.dynamic quit
 /opt/zookeeper/bin/zkServer.sh stop
-sleep infinity
-#/opt/zookeeper/bin/zkServer.sh start-foreground /opt/zookeeper/conf/zoo.cfg
+/opt/zookeeper/bin/zkServer.sh start-foreground /opt/zookeeper/conf/zoo.cfg
