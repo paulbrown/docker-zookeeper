90c22962f7127e950480c9bcda2b3ca39af7e7ff
paulbrown <paul6rown@gmail.com>
2016-09-05 10:46:21 +1000

    onStart.sh modification

--- onStart.sh
+++ onStart.sh
@@ -15,9 +15,16 @@ else
     P_HOSTNAME=$( echo ${PEER} | cut -d "." -f1 )
     P_ID=$( echo ${P_HOSTNAME} | cut -d "-" -f2 | cut -d "-" -f1 )
 
-    echo "server.${P_ID}=${PEER}:2888:3888:participant;2181" >> /opt/zookeeper/conf/zoo.cfg.dynamic
+    if [ "${T_HOSTNAME}" == "${P_HOSTNAME}" ]; then
+      echo "server.${P_ID}=${PEER}:2888:3888:participant;2181" >> /opt/zookeeper/conf/zoo.cfg.dynamic
+      /opt/zookeeper/bin/zkServer-initialize.sh --force --myid=${P_ID}
+      /opt/zookeeper/bin/zkServer.sh start /opt/zookeeper/conf/zoo.cfg
+      /opt/zookeeper/bin/zkCli.sh reconfig -add "server.${P_ID}=${PEER}:2888:3888:participant;2181" quit
+      /opt/zookeeper/bin/zkServer.sh stop
+    else
+      echo "server.${P_ID}=${PEER}:2888:3888:participant;2181" >> /opt/zookeeper/conf/zoo.cfg.dynamic
+    fi
   done
 fi
 
-/opt/zookeeper/bin/zkServer-initialize.sh --force --myid=${T_ID}
 /opt/zookeeper/bin/zkServer.sh start-foreground /opt/zookeeper/conf/zoo.cfg
