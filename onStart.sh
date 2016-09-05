a4ddf72bba249289d45c4fd9bd21fb7f241738fd
paulbrown <paul6rown@gmail.com>
2016-09-05 12:36:32 +1000

    onStart.sh

--- onStart.sh
+++ onStart.sh
@@ -17,19 +17,6 @@ else
 
     echo "server.${P_ID}=${PEER}:2888:3888:participant;2181" >> /opt/zookeeper/conf/zoo.cfg.dynamic
   done
-
-  for PEER in "${PEERS[@]}"
-  do
-    S_HOSTNAME=$( echo ${PEER} | cut -d "." -f1 )
-    S_ID=$( echo ${S_HOSTNAME} | cut -d "-" -f2 | cut -d "-" -f1 )
-
-    if [ "${T_HOSTNAME}" == "${S_HOSTNAME}" ]; then
-      /opt/zookeeper/bin/zkServer-initialize.sh --force --myid=${S_ID}
-      /opt/zookeeper/bin/zkServer.sh start /opt/zookeeper/conf/zoo.cfg
-      /opt/zookeeper/bin/zkCli.sh reconfig -add "server.${S_ID}=${PEER}:2888:3888:participant;2181" quit
-      /opt/zookeeper/bin/zkServer.sh stop
-    fi
-  done
 fi
 
 sleep infinity
