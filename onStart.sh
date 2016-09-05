3e6a59479bbd3bbc4d862a7a587c00e3bf15c189
paulbrown <paul6rown@gmail.com>
2016-09-05 11:31:09 +1000

    update

--- onStart.sh
+++ onStart.sh
@@ -15,14 +15,19 @@ else
     P_HOSTNAME=$( echo ${PEER} | cut -d "." -f1 )
     P_ID=$( echo ${P_HOSTNAME} | cut -d "-" -f2 | cut -d "-" -f1 )
 
-    if [ "${T_HOSTNAME}" == "${P_HOSTNAME}" ]; then
-      echo "server.${P_ID}=${PEER}:2888:3888:participant;2181" >> /opt/zookeeper/conf/zoo.cfg.dynamic
-      /opt/zookeeper/bin/zkServer-initialize.sh --force --myid=${P_ID}
+    echo "server.${P_ID}=${PEER}:2888:3888:participant;2181" >> /opt/zookeeper/conf/zoo.cfg.dynamic
+  done
+
+  for PEER in "${PEERS[@]}"
+  do
+    S_HOSTNAME=$( echo ${PEER} | cut -d "." -f1 )
+    S_ID=$( echo ${P_HOSTNAME} | cut -d "-" -f2 | cut -d "-" -f1 )
+
+    if [ "${T_HOSTNAME}" == "${S_HOSTNAME}" ]; then
+      /opt/zookeeper/bin/zkServer-initialize.sh --force --myid=${S_ID}
       /opt/zookeeper/bin/zkServer.sh start /opt/zookeeper/conf/zoo.cfg
-      /opt/zookeeper/bin/zkCli.sh reconfig -add "server.${P_ID}=${PEER}:2888:3888:participant;2181" quit
+      /opt/zookeeper/bin/zkCli.sh reconfig -add "server.${S_ID}=${PEER}:2888:3888:participant;2181" quit
       /opt/zookeeper/bin/zkServer.sh stop
-    else
-      echo "server.${P_ID}=${PEER}:2888:3888:participant;2181" >> /opt/zookeeper/conf/zoo.cfg.dynamic
     fi
   done
 fi
