9f5eeb3722221fc252740dabdeaf65d1f0f2237d
paulbrown <paul6rown@gmail.com>
2016-09-07 18:10:59 +1000

    update

--- onStart.sh
+++ onStart.sh
@@ -13,10 +13,14 @@ for PEER in "${PEERS[@]}"
 do
   P_HOSTNAME=$( echo ${PEER} | cut -d "." -f1 )
   P_ID=$( echo ${P_HOSTNAME} | cut -d "-" -f2 | cut -d "-" -f1 )
-  echo "server.${P_ID}=${PEER}:2888:3888:participant;2181" >> /opt/zookeeper/conf/zoo.cfg.dynamic
+  P_MEMBER="server.${P_ID}=${PEER}:2888:3888:participant;2181"
+  echo ${P_MEMBER} >> /opt/zookeeper/conf/zoo.cfg.dynamic
+  T_MEMBERS=${T_MEMBERS#\,},${P_MEMBER}
 done
+
+echo ${T_MEMBERS}
 
 /opt/zookeeper/bin/zkServer-initialize.sh --force --myid=${T_ID}
 /opt/zookeeper/bin/zkServer.sh start /opt/zookeeper/conf/zoo.cfg
-/opt/zookeeper/bin/zkCli.sh reconfig -members "server.0=zookeeper-0.zookeeper.default.svc.cluster.local.:2888:3888:participant;2181"
+/opt/zookeeper/bin/zkCli.sh reconfig -members ${T_MEMBERS}
 sleep infinity
